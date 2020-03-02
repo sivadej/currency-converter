e0bf590c8f520dfb944cd848cb2486522639a40f
Sivadej K <bombyk@gmail.com>
2020-03-02 11:26:47 -0900

    staging

--- app.py
+++ app.py
@@ -4,21 +4,6 @@ from currency_converter import *
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret'
 
-### debugtoolbar config
-from flask_debugtoolbar import DebugToolbarExtension
-app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
-toolbar = DebugToolbarExtension(app)
-###
-
-# notes:
-# trying to get rid of using session for form handling.
-# try this - store form data in dictionary, pass to render_template to pre-fill form on submit
-# alternatively, if i have to use session:
-# make sure to perform session handling at every user action
-# store in session right away, then do validation and update session at each necessary step
-
-# get rid of javascript form reset, create a reset route
-
 @app.route('/')
 def show_index():
     """ Main form view """
@@ -26,8 +11,8 @@ def show_index():
 
 @app.route('/convert', methods=['POST'])
 def do_conversion():
-    """ Validates form data before requesting conversion request """
-    create_session ({
+    """ Use form data to request conversion and display resulting message """
+    update_session_from_form ({
         'conv_from' : request.form['convert-from'],
         'conv_to' : request.form['convert-to'],
         'amount' : request.form['amount'],
--- currency_converter.py
+++ currency_converter.py
@@ -5,61 +5,66 @@ from forex_python.converter import CurrencyRates, CurrencyCodes
 # instantiate rate converter
 c = CurrencyRates()
 
-######### how to display multiple errors? 
-######### change all the redirects to just flash message, let app.py handle one message view
+def get_result_msg():
+    if validate_session() is True:
+        return get_conversion_msg()
+    else:
+        return ''
+    
+def get_conversion_msg():
+    """ Returns string with message showing final conversions. Amounts are rounded to two decimal places.
+    This function should only be called with validated inputs stored in session. """
+
+    # Determine and assign currency symbols.
+    s = CurrencyCodes()
+    symbol_from = s.get_symbol(session['convert_from'])
+    symbol_to = s.get_symbol(session['convert_to'])
+
+    # Get converted amount and generate string to be displayed
+    converted_amount = get_converted_amount()
+    success_msg = f"{symbol_from} {round(session['amount'],2)} = {symbol_to} {converted_amount}"
+    return success_msg
 
 def validate_session():
+    """ Checks for non-empty session data, valid amount as Decimal type, and valid currency codes.
+    Flashes message for each error. """
+    is_valid = True
+
+    # Check for empty input fields
     if session['convert_to'] == '' or session['convert_from'] == '' or session['amount'] == '':
-        return 'Please fill in all fields.'
+        flash('Please fill in all fields.')
+        is_valid = False
+
     # Ensure amount input properly converts to Decimal type.
     try:
-        amount = Decimal(session['amount'])
+        session['amount'] = Decimal(session['amount'])
     except:
-        return 'Invalid amount.'
-    session['amount'] = Decimal(session['amount'])
-
+        flash('Invalid amount.')
+        is_valid = False
+    
     # Convert to uppercase currency codes as required by API
     session['convert_from'] = session['convert_from'].upper()
     session['convert_to'] = session['convert_to'].upper()
 
-    # Check for valid country codes via API.
-    # Country codes are called individually in order to determine which input is throwing the error.
+    # Validate currency codes individually in order to determine which input is throwing the error.
     try:
         c.get_rates(session['convert_from'])
     except:
-        return session['convert_from'] + ' is not a valid code.'
+        flash( session['convert_from'] + ' is not a valid code.' )
+        is_valid = False
     try:
         c.get_rates(session['convert_to'])
     except:
-        return session['convert_to'] + ' is not a valid code.'
+        flash( session['convert_to'] + ' is not a valid code.' )
+        is_valid = False
     
-    return True
-
-def get_result_msg():
-    validation_result = validate_session()
-    if validation_result is True:
-        return get_conversion_msg()
-    else:
-        return validation_result
-    
-def get_conversion_msg():
-    """Returns string with message showing final conversions. Amounts are rounded to two decimal places.
-    This function should only be called while valid inputs are stored in session."""
-    # Instantiate currency codes and determine currency symbols.
-    # Error handling should not be necessary on symbol since it
-    #   it assumed valid when country codes were validated.
-    s = CurrencyCodes()
-    symbol_from = s.get_symbol(session['convert_from'])
-    symbol_to = s.get_symbol(session['convert_to'])
-    conversion_result = get_converted_amount()
-    success_msg = f"{symbol_from} {round(session['amount'],2)} = {symbol_to} {conversion_result}"
-    return success_msg
+    return is_valid
 
 def get_converted_amount():
-    converted_amount = c.convert(session['convert_from'], session['convert_to'], session['amount'])
-    return round(converted_amount, 2)
+    amt = c.convert(session['convert_from'], session['convert_to'], session['amount'])
+    return round(amt, 2)
 
-def create_session(form_data):
+def update_session_from_form(form_data):
     session['convert_from'] = form_data['conv_from']
     session['convert_to'] = form_data['conv_to']
     session['amount'] = form_data['amount']
